61d03fb194839ba6ab5bfbe7859c7bc4246a1e03
Divyaa0205 <divya2310084@akgec.ac.in>
2024-11-28 00:46:42 +0530

    checked order after concatenate

--- app.py
+++ app.py
@@ -1,3 +1,70 @@
+# from flask import Flask, request, render_template
+# import pickle
+# import pandas as pd
+
+# app = Flask(__name__)
+
+# model = pickle.load(open('random_forest_model.pkl', 'rb'))
+# scaler = pickle.load(open('scaler.pkl', 'rb'))
+# model_columns = pickle.load(open('model_columns.pkl', 'rb'))
+
+# numerical_cols = ['age', 'education_num', 'capital_gain', 'capital_loss', 'hours_per_week']
+# categorical_cols = ['workclass', 'marital_status', 'occupation', 'relationship', 'race', 'sex']
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     form_data = {key: request.form.get(key) for key in ['age', 'education_num', 'capital_gain', 'capital_loss', 'hours_per_week', 'workclass', 'marital_status', 'occupation', 'relationship', 'race', 'sex']}
+
+#     cat_data = {col: form_data[col] for col in categorical_cols}
+#     cat_df = pd.DataFrame([cat_data])
+#     cat_df = pd.get_dummies(cat_df, columns=categorical_cols, drop_first=False)
+#     # print(cat_df)
+
+#     cat_df = cat_df.reindex(columns=model_columns, fill_value=0)
+#     # print(cat_df)
+#     # print("Input Data Columns:", cat_df.columns)
+#     # print("Model Columns:", model_columns)
+#     # print(set(cat_df.columns) - set(model_columns))
+    
+#     # if list(cat_df.columns) == list(model_columns):
+#     #     print("True")
+#     # else:
+#     #     print("False")
+
+#     for i, (input_col, model_col) in enumerate(zip(cat_df.columns, model_columns)):
+#         if input_col != model_col:
+#             print(f"Column mismatch at position {i}: Input column '{input_col}' does not match model column '{model_col}'.")
+#         else:
+#             print("ALL columns matched ")
+
+#     numerical_data = {col: int(form_data[col]) for col in numerical_cols}
+#     # print(numerical_data)
+#     num_df = pd.DataFrame([numerical_data])
+
+#     input_df = pd.concat([num_df, cat_df], axis=1)
+#     print(input_df.columns)
+#     input_df[numerical_cols] = scaler.transform(input_df[numerical_cols])
+    
+#     input_df = input_df.drop(columns=numerical_cols)
+#     print(input_df.columns)
+#     input_df = input_df.fillna(0)
+
+#     print(input_df)
+#     prediction = model.predict(input_df)[0]
+#     income_prediction = ">50K" if prediction == 1 else "<=50K"
+
+#     return render_template('result.html', prediction=income_prediction)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
 from flask import Flask, request, render_template
 import pickle
 import pandas as pd
@@ -22,44 +89,50 @@ def predict():
     cat_data = {col: form_data[col] for col in categorical_cols}
     cat_df = pd.DataFrame([cat_data])
     cat_df = pd.get_dummies(cat_df, columns=categorical_cols, drop_first=False)
-    # print(cat_df)
 
+   
     cat_df = cat_df.reindex(columns=model_columns, fill_value=0)
-    # print(cat_df)
-    # print("Input Data Columns:", cat_df.columns)
-    # print("Model Columns:", model_columns)
-    # print(set(cat_df.columns) - set(model_columns))
-    
-    # if list(cat_df.columns) == list(model_columns):
-    #     print("True")
-    # else:
-    #     print("False")
 
+    mismatch_found = False
     for i, (input_col, model_col) in enumerate(zip(cat_df.columns, model_columns)):
         if input_col != model_col:
             print(f"Column mismatch at position {i}: Input column '{input_col}' does not match model column '{model_col}'.")
-        else:
-            print("ALL columns matched ")
+            mismatch_found = True
+
+    if not mismatch_found:
+        print("ALL columns matched")
 
     numerical_data = {col: int(form_data[col]) for col in numerical_cols}
-    # print(numerical_data)
     num_df = pd.DataFrame([numerical_data])
 
+   
     input_df = pd.concat([num_df, cat_df], axis=1)
-    print(input_df.columns)
+
+    print("before scaling:", input_df.columns)
+    for i, (input_col, model_col) in enumerate(zip(cat_df.columns, model_columns)):
+        if input_col != model_col:
+            print(f"Column mismatch at position {i}: Input column '{input_col}' does not match model column '{model_col}'.")
+            mismatch_found = True
+
+    if not mismatch_found:
+        print("ALL columns matched")
+
+ 
     input_df[numerical_cols] = scaler.transform(input_df[numerical_cols])
-    
+
+ 
     input_df = input_df.drop(columns=numerical_cols)
-    print(input_df.columns)
+
+   
     input_df = input_df.fillna(0)
 
-    print(input_df)
+    print("after scaling and preprocessing:", input_df.columns)
+
+   
     prediction = model.predict(input_df)[0]
     income_prediction = ">50K" if prediction == 1 else "<=50K"
 
     return render_template('result.html', prediction=income_prediction)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
